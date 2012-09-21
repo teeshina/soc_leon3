acb3a0a0ce43a0e8cf78413a510ac16cc59af5cf
Alex Kosin <alex.kosin@gnss-sensor.com>
2012-09-21 17:51:07 +0400

    delete all

--- source/firmware/HelloWorld/src/gnssengine.cpp
+++
@@ -1,9 +0,0 @@
-#include "gnssengine.h"
-
-void GnssEngine::Init()
-{
-  gnss = (GnssEngine_fields*)ADR_GNSS_ENGINE_BASE;
-
-  gnss->gtmr.rw_MsLength = 7000;
-  for (int32 i=0; i<CFG_GNSS_CHANNELS_TOTAL; i++) gnss->chan[i].rw_dbgIncr = 0xff123400+i; // 
-}
--- source/firmware/HelloWorld/src/stdtypes.h
+++
@@ -1,7 +0,0 @@
-#ifndef __STDTYPES_H__
-#define __STDTYPES_H__
-
-typedef unsigned int uint32;
-typedef int int32;
-
-#endif
